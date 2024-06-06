0ef66ff61b78a053405664b1d164de25b2f1366c
archchipunza <128588819+archchipunza@users.noreply.github.com>
2024-06-06 10:50:15 +0200

    Initial commit

---
+++ src/main/java/pl/edu/vistula/java_59644/Java59644Application.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.java_59644;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java59644Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java59644Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/vistula/java_59644/Java59644ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.java_59644;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java59644ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
